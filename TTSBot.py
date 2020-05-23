e0b472ddb3db38e05d7ee526d5e9752222d7fd87
Muehli25 <Muehli25@users.noreply.github.com>
2020-05-23 10:59:45 +0200

    Revert "Merge pull request #1 from n-gao/patch-1"
    This has issues with playback

--- TTSBot.py
+++ TTSBot.py
@@ -1,6 +1,6 @@
 import discord
 
-from aiogtts import aiogTTS
+from gtts import gTTS
 import uuid
 
 from discord_token import TOKEN
@@ -12,7 +12,6 @@ class TTSBot(discord.Client):
         self.CurrentConnection = None
         self.queue = []
         self.play_next()
-        self.tts = aiogTTS()
 
     async def on_ready(self):
         print('Logged in as {0.user}'.format(client))
@@ -80,7 +79,8 @@ class TTSBot(discord.Client):
                 try:
                     print(f"Create mp3 for text: {text} in {lang}")
                     filename = f'data/{name}.mp3'
-                    await self.tts.save(text, filename, lang=lang)
+                    tts = gTTS(text, lang=lang)
+                    tts.save(filename)
                     self.queue.append(filename)
                     # self.play(discord.FFmpegPCMAudio(filename))
                 except ValueError:
