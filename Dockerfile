3b498adbe035ab421853fdf54b2a19577d440d58
Lars Mühlenstädt <muehlenstaedt.lars@gmail.com>
2020-08-17 18:37:39 +0200

    remove old bot remove unused files and folders rename Dockerfile and python file

--- Dockerfile
+++ Dockerfile
@@ -13,6 +13,6 @@ WORKDIR /usr/src/app
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
-COPY TTSBot.py .
+COPY CallableTTSBot.py .
 
-CMD [ "python", "TTSBot.py" ]
+CMD [ "python", "CallableTTSBot.py" ]
--- TTSBot.py
+++ TTSBot.py
@@ -3,44 +3,55 @@ import discord
 from gtts import gTTS
 import uuid
 import os
+import queue
 
-SAY_COMMAND = "!say"
 DATA_FOLDER = "data"
 
 
 class TTSBot(discord.Client):
-    def __init__(self):
+
+    def __init__(self, language="en"):
         super().__init__()
         if not os.path.exists(DATA_FOLDER):
             os.makedirs(DATA_FOLDER)
         self.CurrentConnection = None
-        self.queue = []
+        self.language = language
+        self.queue = queue.Queue()
+        self.play_next()
 
-    async def on_ready(self):
-        print('Logged in as {0.user}'.format(client))
-
-    async def join_channel(self, channel):
+    async def call_bot(self, channel):
         self.CurrentConnection = await channel.connect()
         self.play_next()
 
-    async def disconnect_channel(self):
+    async def goodbye_bot(self):
         await self.CurrentConnection.disconnect()
         self.CurrentConnection = None
+        self.queue = queue.Queue()
+
+    async def on_ready(self):
+        print('Logged in as {0.user}'.format(client))
 
     def delete_file(self, filename):
         os.remove(filename)
 
     def play_next(self):
-        if self.CurrentConnection is not None and not self.CurrentConnection.is_playing() and len(self.queue) > 0:
-            to_play = self.queue.pop()
+        if self.CurrentConnection is not None and not self.CurrentConnection.is_playing() and not self.queue.empty():
+            to_play = self.queue.get()
             self.CurrentConnection.play(discord.FFmpegPCMAudio(to_play),
                                         after=lambda e: self.delete_file(filename=to_play))
         if self.CurrentConnection is not None:
             self.loop.call_later(0.5, self.play_next)
 
     def abort_playback(self):
+        file = ""
         self.CurrentConnection.stop()
-        self.queue = []
+        while True:
+            try:
+                file = self.queue.get(block=False)
+            except queue.Empty:
+                break
+        self.delete_file(file)
+        self.queue = queue.Queue()
 
     def play(self, file):
         self.CurrentConnection.play(file)
@@ -49,54 +60,42 @@ class TTSBot(discord.Client):
         if message.author == self.user:
             return
 
-        if message.content.startswith('!bye') and self.CurrentConnection is not None \
+        elif message.content == "!call":
+            await message.channel.send('Hello!')
+            await self.call_bot(message.author.voice.channel)
+
+        elif message.content == '!bye' and self.CurrentConnection is not None \
                 and self.CurrentConnection.is_connected():
             await message.channel.send('Goodbye!')
-            await self.disconnect_channel()
+            await self.goodbye_bot()
 
-        if message.content.startswith("!lang"):
-            await message.channel.send('Visit: {}'.format("https://sites.google.com/site/opti365/translate_codes"))
-
-        if message.content.startswith("!abort"):
+        elif message.content == "!abort":
             self.abort_playback()
 
-        if message.content.startswith(SAY_COMMAND):
-            # if already connected ignore this
-            if self.CurrentConnection is None:
-                await self.join_channel(message.author.voice.channel)
-
-            if self.CurrentConnection.channel != message.author.voice.channel:
-                await self.disconnect_channel()
-                await self.join_channel(message.author.voice.channel)
-
-            # Create uuid as filename
-            name = uuid.uuid1()
-            # Get user input
-            user_input = message.content[len(SAY_COMMAND):]
-            if len(user_input) == 0 or (user_input[0] == ":" and user_input.find(" ") == -1):
-                await message.channel.send('No text provided.')
-            else:
-                # Check for language tag,
-                if user_input[0] == ":":
+        else:
+            if self.CurrentConnection is not None \
+                    and self.CurrentConnection.is_connected():
+                lang = self.language
+                text = user_input = message.content
+                if user_input[0] == "+":
                     divider = user_input.find(" ")
                     lang = user_input[1:divider]
                     text = user_input[(divider + 1):]
-                else:
-                    lang = 'de'
-                    text = user_input[1:]
+                # Create uuid as filename
+                name = uuid.uuid1()
                 # Play the requested text
                 try:
-                    print(f"Create mp3 for text: {text} in {lang}")
                     filename = f'{DATA_FOLDER}/{name}.mp3'
                     tts = gTTS(text, lang=lang)
                     tts.save(filename)
-                    self.queue.append(filename)
-                    # self.play(discord.FFmpegPCMAudio(filename))
+                    self.queue.put(filename)
                 except ValueError:
-                    await message.channel.send(f"Language {lang} not supported.")
+                    await message.channel.send(f"Language not supported or no Text provided.")
 
 
 # Create new bot
 client = TTSBot()
+if os.getenv("LANG") is not None:
+    client = TTSBot(os.environ['LANG'])
 # run Bot with provided discord token
 client.run(os.environ['TOKEN'])
